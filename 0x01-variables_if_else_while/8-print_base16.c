baa7852eec5d0d3e45d1b27fafcd31c4a0224f6f
JoE11-y <josephedoh77@gmail.com>
2022-09-11 23:47:51 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = '0';
+	int hexa = 'a';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (hexa <= 'f')
+	{
+		putchar(hexa);
+		hexa++;
+	}
+
+	putchar('\n');
+	return (0);
+}
