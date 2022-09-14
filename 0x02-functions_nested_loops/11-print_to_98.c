04f0bf4e7ffc29456e0958be04334df80d18f3e9
JoE11-y <josephedoh77@gmail.com>
2022-09-14 01:42:47 -0700

    answer

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,82 +1,23 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: number to start printing from
+ * print_to_98 - print numbers from n to 98
+ * @n: number to print from
  * Return: void
  */
 
 void print_to_98(int n)
 {
-	int i, j, k;
-
 	if (n > 98)
 	{
-		while (n >= 98)
-		{
-			if (n >= 100)
-			{
-				i = n / 100;
-				j = (n / 10) - 10;
-			}
-			else
-			{
-				i = 0;
-				j = n / 10;
-			}
-			k = n % 10;
-			if (i > 0)
-				_putchar(i + '0');
-			_putchar(j + '0');
-			_putchar(k + '0');
-			if (n > 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			n--;
-		}
-		_putchar('\n');
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 	else
 	{
-		while (n <= 98)
-		{
-			if (n < 0)
-			{
-				_putchar('-');
-				if ((n * -1) >= 100)
-				{
-					i = (n / 100) * -1;
-					j = ((n / 10) * -1) - 10;
-					_putchar(i + '0');
-					_putchar(j + '0');
-				}
-				else
-				{
-					j = (n / 10) * -1;
-
-					if (j > 0)
-						_putchar(j + '0');
-				}
-				k = (n % 10) * -1;
-				_putchar(k + '0');
-			}
-			else
-			{
-				j = n / 10;
-				k = n % 10;
-				if (j > 0)
-					_putchar(j + '0');
-				_putchar(k + '0');
-			}
-			if (n < 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			n++;
-		}
-		_putchar('\n');
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
---
+++ 0x02-functions_nested_loops/11-print_to_98_with_putchar.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: number to start printing from
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i, j, k;
+
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n >= 100)
+			{
+				i = n / 100;
+				j = (n / 10) - 10;
+			}
+			else
+			{
+				i = 0;
+				j = n / 10;
+			}
+			k = n % 10;
+			if (i > 0)
+				_putchar(i + '0');
+			_putchar(j + '0');
+			_putchar(k + '0');
+			if (n > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			if (n < 0)
+			{
+				_putchar('-');
+				if ((n * -1) >= 100)
+				{
+					i = (n / 100) * -1;
+					j = ((n / 10) * -1) - 10;
+					_putchar(i + '0');
+					_putchar(j + '0');
+				}
+				else
+				{
+					j = (n / 10) * -1;
+
+					if (j > 0)
+						_putchar(j + '0');
+				}
+				k = (n % 10) * -1;
+				_putchar(k + '0');
+			}
+			else
+			{
+				j = n / 10;
+				k = n % 10;
+				if (j > 0)
+					_putchar(j + '0');
+				_putchar(k + '0');
+			}
+			if (n < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+		_putchar('\n');
+	}
+}
